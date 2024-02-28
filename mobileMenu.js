6a24f1970491f0eb17ab184c3b205a9ad3a9f6ae
MaruyYak <maryana.lus@gmail.com>
2024-02-28 22:06:05 +0300

    fix touchclick

--- mobileMenu.js
+++ mobileMenu.js
@@ -7,26 +7,28 @@ function burgermenuOnload() {
   const burgerMenu = document.querySelector('.burger_menu');
   const navOptions = document.querySelector('.nav_options');
 
-  // burgerMenu.addEventListener('click', function() {
-  //     this.classList.toggle('active');
-  //     navOptions.classList.toggle('open');
-  // })
-  // document.addEventListener('click', function(event) {
-  //     if (!event.target.classList.contains('nav_options')) {
-  //         navOptions.classList.toggle('open');
-  //         burgerMenu.classList.toggle('active');
-  //     }
-  // })
-
-  burgerMenu.addEventListener('touchstart', function(event) {
-    navOptions.classList.toggle('open');
-    burgerMenu.classList.toggle('active');
-    console.log("работает", event)
+  burgerMenu.addEventListener('click', function() {
+      this.classList.toggle('active');
+      navOptions.classList.toggle('open');
+      console.log("работает2", navOptions)
   })
-  document.addEventListener('o', function(event) {
-      if (!event.target.contains('.nav_options') && !event.target.contains('.burger_menu')) {
+
+  navOptions.addEventListener('click', function(event) {
+      if (!event.target.classList.contains('nav_options')) {
           navOptions.classList.toggle('open');
           burgerMenu.classList.toggle('active');
       }
   })
+
+  // burgerMenu.addEventListener('touchstart', function(event) {
+  //   navOptions.classList.toggle('open');
+  //   burgerMenu.classList.toggle('active');
+  //   console.log("работает", event)
+  // })
+  // document.addEventListener('o', function(event) {
+  //     if (!event.target.contains('.nav_options') && !event.target.contains('.burger_menu')) {
+  //         navOptions.classList.toggle('open');
+  //         burgerMenu.classList.toggle('active');
+  //     }
+  // })
 }
