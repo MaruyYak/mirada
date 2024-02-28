555689716d01e9463c3166b4367e9b0a6c91eb2f
MaruyYak <maryana.lus@gmail.com>
2024-02-28 21:53:35 +0300

    fix: touch menu cklick

--- mobileMenu.js
+++ mobileMenu.js
@@ -7,21 +7,21 @@ function burgermenuOnload() {
   const burgerMenu = document.querySelector('.burger_menu');
   const navOptions = document.querySelector('.nav_options');
 
-  burgerMenu.addEventListener('click', function() {
-      this.classList.toggle('active');
-      navOptions.classList.toggle('open');
-  })
-  document.addEventListener('click', function(event) {
-      if (!event.target.classList.contains('nav_options')) {
-          navOptions.classList.toggle('open');
-          burgerMenu.classList.toggle('active');
-      }
-  })
+  // burgerMenu.addEventListener('click', function() {
+  //     this.classList.toggle('active');
+  //     navOptions.classList.toggle('open');
+  // })
+  // document.addEventListener('click', function(event) {
+  //     if (!event.target.classList.contains('nav_options')) {
+  //         navOptions.classList.toggle('open');
+  //         burgerMenu.classList.toggle('active');
+  //     }
+  // })
 
-  burgerMenu.addEventListener('touchend', function(event) {
+  burgerMenu.addEventListener('touchstart', function(event) {
     navOptions.classList.toggle('open');
     burgerMenu.classList.toggle('active');
-    console.log("работает")
+    console.log("работает", event)
   })
   document.addEventListener('o', function(event) {
       if (!event.target.contains('.nav_options') && !event.target.contains('.burger_menu')) {
