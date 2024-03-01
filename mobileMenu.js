2bb5138da7096579abdd4d07976993aa13a2bca9
MaruyYak <maryana.lus@gmail.com>
2024-03-02 00:36:52 +0300

    feat: add footer info and tooltip

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,10 +1,12 @@
 window.onload = function () { 
-  burgermenuOnload()
+  burgermenuOnload();
+  addTooltip();
 }  
 
 function burgermenuOnload() {
   const burgerMenu = document.querySelector('.burger_menu');
   const navOptions = document.querySelector('.nav_options');
+  const body = document.querySelector('.site_body')
 
   burgerMenu.addEventListener('click', function() {
       this.classList.toggle('active');
@@ -13,9 +15,26 @@ function burgermenuOnload() {
 
   navOptions.addEventListener('click', function(event) {
       if (!event.target.classList.contains('nav_options')) {
-          navOptions.classList.toggle('open');
-          burgerMenu.classList.toggle('active');
+          navOptions.classList.remove('open');
+          burgerMenu.classList.remove('active');
       }
   })
 
 }
+
+function addTooltip() {
+  const tooltips = document.querySelectorAll('.tooltipHave');
+  
+  tooltips.forEach(tooltip => {
+    const tooltipText = tooltip.nextElementSibling;
+    tooltip.addEventListener('mouseover', () => {
+      tooltipText.style.visibility = 'visible';
+    });
+
+    tooltip.addEventListener('mouseout', () => {
+      tooltipText.style.visibility = 'hidden';
+    });
+  });
+
+}
+
